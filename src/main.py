e0e7b5537fc5f986b3b67eb96fb465876ab188bf
of15641 <of15641@bristol.ac.uk>
2020-11-01 21:56:35 +0000

    add database linking and inputs. Pretty much done!

--- src/main.py
+++ src/main.py
@@ -1,9 +1,44 @@
-from .recording import *
+import src.recording.recording as logs
 
-if __name__ == '__main__':
+def get_input(move, player):
+    m_c = input("Move: %s, Player: %s -- move and comment: " % (move, player)).split(" ")
+
+    try:
+        assert(len(m_c) == 2 or len(m_c) == 1)
+    except:
+        print("only input move and comment, like 'b4 !!'")
+        exit(1)
+    else:
+        if len(m_c) == 1:
+            return [m_c[0], "-"]
+        elif len(m_c) == 2:
+            return m_c
+
+
+def main():
     # open connection to recording
-
-    #initialize board
+    this_game = logs.GameLog()
 
     #loop through moves until checkmate or draw
+    game_on = True
+    move = 1
+    players = ["black", "white"]
+    player = players[move]
+    while(game_on):
+        [m, c] = get_input(move, player)
+
+        this_game.add_move(player, m, c)
+
+        if "#" in m or "++" in m:
+            game_on = False
+            continue
+
+        move += 1
+        player = players[move % 2]
+
+    print("%s wins in %s moves" % (player, str(move)))
+
+
+if __name__ == '__main__':
+    main()
     exit(0)
--- src/recording/recording.py
+++ src/recording/recording.py
@@ -1,11 +1,86 @@
 import datetime
+import sqlite3
+import os
 
-class log:
+class GameLog:
     '''
-    >>> test_log = log()
-    >>> assert(test_log.date == datetime.date.today())
+    >>> import time
+    >>> test_log = GameLog("test_table")
+
+    >>> time.sleep(1)
+    >>> test_log.add_move("b4")
+
     '''
 
-    def __init__(self):
-        self.date = datetime.date.today()
+    def connect_to_db(self):
+        assert(os.path.exists("chess_games_log.db"))
+        return sqlite3.connect("chess_games_log.db")
 
+    def make_new_table(self, table_name):
+        '''
+        take the current date and time and make a new table.
+
+        Due to SQL injection attacks (probably not an issue here tbf)
+        it is better to use parameter subsitution rather than using
+        python string operations
+
+        Turns out table names cannot use parameter substitution. Previous
+        code left here for notes:
+
+        start_time = (datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S'),)
+        c = self.conn.cursor()
+
+        c.execute("CREATE TABLE ? (move time comment)", start_time)
+
+
+        Turns out using the date as the name of a table is also a bad
+        idea.
+
+        '''
+        c = self.conn.cursor()
+
+        if not table_name:
+            game_no = c.execute("SELECT count(*) FROM sqlite_master WHERE type = 'table'")
+
+            game_name = "game_%s" % game_no.fetchone()[0]
+        else:
+            all_games = c.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
+
+            all_similar_games = [x for x in all_games.fetchall() if table_name in x[0]]
+
+            game_no = len(all_similar_games)
+
+            game_name = "%s_%s" % (table_name, str(game_no))
+
+        table_command = "CREATE TABLE %s (player text, move text, time text, comment text)" % game_name
+
+        c.execute(table_command)
+
+        self.conn.commit()
+
+        return game_name
+
+
+    def __init__(self, table_name = ""):
+        self.date = datetime.datetime.today()
+        self.conn = self.connect_to_db()
+
+        self.game_name = self.make_new_table(table_name)
+
+
+
+    def add_move(self, player, move_string, comment_string = "-",):
+        allowed_comments = ["-", "!", "!!", "?", "??", "?!", "!?"]
+        assert(comment_string in allowed_comments)
+
+        time_diff = datetime.datetime.today() - self.date
+
+        c = self.conn.cursor()
+
+        parameters = (player, move_string, time_diff.seconds, comment_string)
+
+        command = "INSERT INTO %s VALUES (?, ?, ?, ?)" % self.game_name
+
+        c.execute(command, parameters)
+
+        self.conn.commit()
